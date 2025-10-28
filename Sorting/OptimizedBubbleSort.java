a21c7403817458ac7336c9ae157c3df78ee6c5b1
Saniya Mane <185913688+Saniya2701@users.noreply.github.com>
2025-10-28 23:23:01 +0530

    Add files via upload

---
+++ Sorting/OptimizedBubbleSort.java
@@ -0,0 +1,47 @@
+package Sorting;
+import java.util.*;
+public class OptimizedBubbleSort {
+
+    public static void Optimized_Bubble(int arr[],int n){
+        for(int i =0;i<n-1 ; i++){
+            boolean isTrue = false;
+        for(int j=0;j<n-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                //swap
+
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+
+                isTrue= true;
+
+
+            }
+        }
+
+      }
+    }
+
+    public static void Printarr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]+ " ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter size of array:");
+        int n= sc.nextInt();
+
+        int arr[]=new int[n];
+
+        System.out.println("Enter array elements:");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+
+        }
+
+        Optimized_Bubble(arr,n);
+        Printarr(arr);
+    }
+}
