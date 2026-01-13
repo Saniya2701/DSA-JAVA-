5d360ec631c665ddd3035c4a98dd66e2db3b5c54
Saniya Mane <185913688+Saniya2701@users.noreply.github.com>
2026-01-13 15:35:13 +0530

    Add files via upload

---
+++ Stack/Duplicate_Parentheses.java
@@ -0,0 +1,38 @@
+//google,microsoft
+//TC = O(n)
+//Q. the given string is a valid string check it has a duplicate parentheses or nnot
+package Stack;
+import java.util.*;
+public class Duplicate_Parentheses {
+
+
+    public static boolean isDuplicate(String str){
+        Stack<Character> s = new Stack<>();
+
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+
+            if(ch == ')'){
+                int count =0;
+                while(s.peek()!='('){
+                    s.pop();
+                    count++;
+                }
+                if(count<1){
+                    return true;//has duplicate parentheses
+                }else{
+                    s.pop();
+                }
+            }else{
+                s.push(ch);
+            }
+        }
+        return false;
+    }
+    public static void main(String args[]){
+        String str ="((a+b))";//true
+        String str2 ="(a-b)";//false
+
+        System.out.println(isDuplicate(str2));
+    }
+}
