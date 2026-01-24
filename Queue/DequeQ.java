58d44a4dea985cf702eabb36558ba8350654c3b3
Saniya Mane <185913688+Saniya2701@users.noreply.github.com>
2026-01-24 23:01:00 +0530

    Add files via upload

---
+++ Queue/DequeQ.java
@@ -0,0 +1,18 @@
+package Queue;
+import java.util.*;
+public class DequeQ {
+    public static void main(String args[]){
+        Deque<Integer> d = new LinkedList<>();
+        d.addFirst(1);
+        d.addFirst(2);
+        d.addLast(3);
+        d.addLast(4);
+        System.out.println(d);
+        d.removeLast();
+        System.out.println(d);
+
+        System.out.println("first element :" + d.getFirst());
+        System.out.println("Last element:"+ d.getLast());
+
+    }
+}
